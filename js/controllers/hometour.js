069fc1bfecf1ce8b7445e93c8d79475b26184c15
chrisalexander <chris.alexander@import.io>
2013-10-12 14:20:34 +0100

    Home tour

--- js/controllers/hometour.js
+++ js/controllers/hometour.js
@@ -17,6 +17,57 @@ ll.controller("HomeTour", ["$scope",
 		}
 		
 		$scope.view = $scope.tours[type][0];
+		$scope.currentTour = type;
+	}
+
+	// Jump to next tour step
+	$scope.nextTour = function() {
+		if (!$scope.currentTour) {
+			return;
+		}
+		for (var i = 0; i < $scope.tours[$scope.currentTour].length; i++) {
+			if ($scope.tours[$scope.currentTour][i] == $scope.view) {
+				if (i == $scope.tours[$scope.currentTour].length - 1) {
+					// If its the last one, then we need to go to signup
+					$scope.signup();
+				} else {
+					// Otherwise, go to the next one
+					$scope.view = $scope.tours[$scope.currentTour][i+1];
+				}
+				break;
+			}
+		}
+	}
+
+	// Jump to previous tour step
+	$scope.prevTour = function() {
+		if (!$scope.currentTour) {
+			return;
+		}
+		for (var i = 0; i < $scope.tours[$scope.currentTour].length; i++) {
+			if ($scope.tours[$scope.currentTour][i] == $scope.view) {
+				if (i == 0) {
+					// If its the last one, then we need to go to signup
+					$scope.goHome();
+				} else {
+					// Otherwise, go to the next one
+					$scope.view = $scope.tours[$scope.currentTour][i-1];
+				}
+				break;
+			}
+		}
+	}
+
+	// Goes home
+	$scope.goHome = function() {
+		$scope.view = "home";
+		$scope.currentTour = false;
+	}
+
+	// Go to signup
+	$scope.signup = function() {
+		$scope.view = "signup";
+		$scope.currentTour = false;
 	}
 	
 }]);
