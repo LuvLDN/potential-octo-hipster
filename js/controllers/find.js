4ae732745629f41b1f67d555e111aaf2c4c12835
chrisalexander <chris.alexander@import.io>
2013-10-13 12:42:31 +0100

    HERE MAPS is weird

--- js/controllers/find.js
+++ js/controllers/find.js
@@ -27,7 +27,7 @@ ll.controller("Find", ["$scope", "elasticsearch", "safeApply",
 				var data = d.hits.hits;
 				data.map(function(obj) { $scope.outlets.push(obj['_source']) });
 				$scope.outlets.map(function(outlet) {
-					var marker = new nokia.maps.map.StandardMarker([outlet.location.lat, outlet.location.lon], {
+					var marker = new nokia.maps.map.StandardMarker([parseFloat(outlet.location.lat), parseFloat(outlet.location.lon)], {
 						text: outlet.name,
 						draggable: false
 					});
